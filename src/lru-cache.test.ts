e3898d8626874f3c54a1ac8022380e45ed7b352d
Thiago Martins <thiago_valentim1@hotmail.com>
2024-02-29 19:30:35 -0300

    feat(lru): returns null for unknown key

--- src/lru-cache.test.ts
+++ src/lru-cache.test.ts
@@ -1,7 +1,30 @@
 import { describe, test } from "node:test";
+import { Cache } from "./cache.interface";
+import assert from 'node:assert/strict';
 
 describe('LRU Cache', () => {
-  test.todo('returns null for unknown keys');
+  test('returns null for unknown keys', () => {
+    const cache: Cache = LRUCache.ofSize(3);
+
+    assert.equal(cache.get('foo'), null);
+  });
   test.todo('returns the value for known keys');
   test.todo('evicts the least recently used key when at capacity');
 });
+
+class LRUCache implements Cache {
+  private constructor(private size: number) { }
+
+  get(key: string): string | null {
+    return null;
+  }
+  set(key: string, value: string): void {
+    throw new Error("Method not implemented.");
+  }
+  del(key: string): void {
+    throw new Error("Method not implemented.");
+  }
+  public static ofSize(size: number): Cache { 
+    return new LRUCache(size);
+  }
+}
